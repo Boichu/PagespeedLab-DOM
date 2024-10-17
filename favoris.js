503a7296fc1055076bd90a379f252cb550718fe2
Gaetan_Boishue <gaetan@wwwup.fr>
2024-10-17 10:46:55 +0200

    pour l'ajout en favoris

---
+++ favoris.js
@@ -0,0 +1,8 @@
+javascript:(
+    function(){
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = "https://cdn.jsdelivr.net/gh/Boichu/PagespeedLab-DOM/content.js";
+        document.body.appendChild(script);
+    }
+)();
